cb5bceddfd1afc7647e263159300ffdc3df290cd
Bob Liu <liuyingxuan@gmail.com>
2018-04-16 11:09:16 -0500

    Swapped node-sass for node-sass-middleware

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const sass = require('node-sass');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const express = require('express')
+const path = require('path')
+const sassMiddleware = require('node-sass-middleware')
+const PORT = process.env.PORT || 5000
 
 express()
-  .set('views', path.join(__dirname, 'views'));
-  .set('view engine', 'ejs');
-  .get('/', (req, res) => res.render('pages/index'));
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
   .use(
-    sass.middleware({
+    sassMiddleware({
       src: __dirname + '/sass',
       dest: __dirname + '/public/stylesheets',
       prefix:  '/stylesheets',
       debug: true,
     })
-  );
-  .use(express.static(path.join(__dirname, 'public')));
   )
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
