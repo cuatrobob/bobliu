2a30a12ad8c007e0d851a91c746949613cf9e5ad
Bob Liu <liuyingxuan@gmail.com>
2018-04-16 09:50:15 -0500

    Modified package/app.json; added node-sass

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const sass = require('node-sass');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .set('views', path.join(__dirname, 'views'));
+  .set('view engine', 'ejs');
+  .get('/', (req, res) => res.render('pages/index'));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use(
+    sass.middleware({
+      src: __dirname + '/sass',
+      dest: __dirname + '/public/stylesheets',
+      prefix:  '/stylesheets',
+      debug: true,
+    })
+  );
+  .use(express.static(path.join(__dirname, 'public')));
+  )
